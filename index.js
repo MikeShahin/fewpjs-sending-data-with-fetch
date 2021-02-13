564b45a8ee1c3975bcad2ec8837bb373f2f82179
MikeShahin <gobias101@gmail.com>
2021-02-13 15:23:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-// Add your code here
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// }); 
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+  
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configObj)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(object) {
+//     console.log(object);
+//   })
+//   .catch(function(error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   }); 
+
+  function submitData(name, email) {
+      return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data => {
+          document.body.innerHTML = data["id"]
+          console.log(data)
+        })
+       .catch(function(error) {
+            document.body.innerHTML = error.message
+      }); 
+  }
+  submitData("mike", "adad")
